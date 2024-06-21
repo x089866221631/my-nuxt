264797d191ca9bb4f39674e1af29751300f1dcb5
Ankh <1725450045@qq.com>
2024-06-22 00:04:09 +0800

    视图

--- app.vue
+++ app.vue
@@ -8,11 +8,8 @@
 <script setup lang="ts">
 useSeoMeta({
   title: "nuxt测试",
-  ogTitle: "我的神奇网站",
   description: "这是我的神奇网站，让我告诉你关于它的一切。",
-  ogDescription: "这是我的神奇网站，让我告诉你关于它的一切。",
-  ogImage: "https://example.com/image.png",
-  twitterCard: "summary_large_image",
+  keywords: "nuxt,测试,网站",
 });
 </script>
 <style scoped></style>
