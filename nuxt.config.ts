535721a7a2306f966dd307c3fbd7bbddb54500cf
Ankh <1725450045@qq.com>
2024-06-22 00:26:53 +0800

    vant安装

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@vant/nuxt"],
   // 开启ssr服务器渲染,默认开启
-  ssr:true,
-  devtools: { enabled: true }
-})
+  ssr: true,
+  // 开启调试工具
+  devtools: { enabled: true },
+});
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,8 @@
 <template>
   <h2>首页</h2>
+  <van-button type="primary" @click="showToast('toast')">button</van-button>
+  <VanButton type="success" @click="showNotify('notify')">button</VanButton>
+  <LazyVanButton type="default">lazy button</LazyVanButton>
 </template>
 
 <script setup lang="ts"></script>
